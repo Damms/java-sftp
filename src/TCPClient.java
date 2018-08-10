88e56d024700a8cf48ed49822074b242315cb009
Jaedyn Damms <j.damms@hotmail.com>
2018-08-11 01:46:48 +1200

    client server communication

--- src/TCPClient.java
+++ src/TCPClient.java
@@ -9,33 +9,43 @@ import java.net.*;
 
 class TCPClient { 
     
+    String[] sftpCommands = {"USER", "ACCT", "PASS", "TYPE", "LIST", "CDIR", "KILL", "NAME", "DONE", "RETR", "STOR"};
+    
     public static void main(String argv[]) throws Exception 
     { 
         String sentence; 
         String modifiedSentence; 
-	
-        BufferedReader inFromUser = 
-	    new BufferedReader(new InputStreamReader(System.in)); 
-	
-        Socket clientSocket = new Socket("hostname", 6789); 
-	
-        DataOutputStream outToServer = 
-	    new DataOutputStream(clientSocket.getOutputStream()); 
-	
         
-	BufferedReader inFromServer = 
-	    new BufferedReader(new
-		InputStreamReader(clientSocket.getInputStream())); 
+        Socket clientSocket = new Socket("localhost", 6789); 
+        
+        while(true){
 	
-        sentence = inFromUser.readLine(); 
+            BufferedReader inFromUser = 
+                new BufferedReader(new InputStreamReader(System.in)); 
+
+            
+
+            DataOutputStream outToServer = 
+                new DataOutputStream(clientSocket.getOutputStream()); 
+
+
+            BufferedReader inFromServer = 
+                new BufferedReader(new
+                    InputStreamReader(clientSocket.getInputStream())); 
+
+            sentence = inFromUser.readLine(); 
+
+            outToServer.writeBytes(sentence + '\n'); 
+
+            modifiedSentence = inFromServer.readLine(); 
+
+            System.out.println("FROM SERVER: " + modifiedSentence); 
+            
+            //break;
+        }
 	
-        outToServer.writeBytes(sentence + '\n'); 
-	
-        modifiedSentence = inFromServer.readLine(); 
-	
-        System.out.println("FROM SERVER: " + modifiedSentence); 
-	
-        clientSocket.close(); 
+        //clientSocket.close(); 
+        
 	
     } 
 } 
--- src/TCPServer.java
+++ src/TCPServer.java
@@ -10,22 +10,23 @@ import java.net.*;
 class TCPServer { 
     
     public static void main(String argv[]) throws Exception 
+    
     { 
 	String clientSentence; 
 	String capitalizedSentence; 
 	
 	ServerSocket welcomeSocket = new ServerSocket(6789); 
+        
+        Socket connectionSocket = welcomeSocket.accept(); 
+
+        BufferedReader inFromClient = 
+            new BufferedReader(new
+                InputStreamReader(connectionSocket.getInputStream())); 
+
+        DataOutputStream  outToClient = 
+            new DataOutputStream(connectionSocket.getOutputStream()); 
 	
 	while(true) { 
-	    
-            Socket connectionSocket = welcomeSocket.accept(); 
-	    
-	    BufferedReader inFromClient = 
-		new BufferedReader(new
-		    InputStreamReader(connectionSocket.getInputStream())); 
-	    
-	    DataOutputStream  outToClient = 
-		new DataOutputStream(connectionSocket.getOutputStream()); 
 	    
 	    clientSentence = inFromClient.readLine(); 
 	    
